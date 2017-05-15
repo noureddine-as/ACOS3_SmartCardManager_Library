fe9475e30831394343a7f25568395156009f3c5e
noureddine-as <noureddine.asni@gmail.com>
2017-05-15 11:03:10 +0100

    Project Created - Using NetBeans IDE

---
+++ src/acos3_smartcardmanager_library/ACOS3_SmartCardManager_Library.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package acos3_smartcardmanager_library;
+
+/**
+ *
+ * @author AIT SAID
+ */
+public class ACOS3_SmartCardManager_Library {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
